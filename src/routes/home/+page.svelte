d1b08718df5472b7a9c8879882b781292efbe953
brennalo <tybrennalo@gmail.com>
2025-02-15 14:30:16 +0800

    log-in page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,9 +1,22 @@
-<script>
-    let i = 0
-</script>
+<!-- <script lang="ts">
+    import label from '$lib/components/label.svelte';
+</script> -->
 
-<div>
-    <p>{i}</p>
-    <p>this is the home page</p>
-    <button on:click={() => i++}>plyu</button>
+<div class = "flex justify-center items-center h-screen bg-gray-600">
+    <div class = "w-96 p-6 shadow-lg bg-white rounded-md">
+
+        <h1 class="text-3xl block text-center font-semibold">Login</h1>
+        <hr class="mt-3">
+        <div class="mt-3">
+            <label for = "contact" class="block" text-base mb-2> Phone Number </label>
+            <input type = "number" id="contact" class="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Enter Phone Number">
+        </div>
+        <div class="mt-3">
+            <label for = "email" class="block" text-base mb-2> Email </label>
+            <input type = "email" id="email" class="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600" placeholder="Enter Password">
+        </div>
+
+    </div>
+
+
 </div>
