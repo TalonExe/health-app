e1b2418fcedff2c4ca5d270b0f16209a4c05f72c
brennalo <tybrennalo@gmail.com>
2025-02-16 02:55:23 +0800

    menu

---
+++ src/lib/components/ui/card/MenuUser.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Card from "$lib/components/ui/card/card.svelte";
+    import CardHeader from "$lib/components/ui/card/card-header.svelte";
+    import CardTitle from "$lib/components/ui/card/card-title.svelte";
+    import CardDescription from "$lib/components/ui/card/card-description.svelte";
+    import CardContent from "$lib/components/ui/card/card-content.svelte";
+    import CardFooter from "$lib/components/ui/card/card-footer.svelte";
+
+    let props = $props();
+</script>
+
+<Card class="bg-white m-3 p-3">
+    <CardHeader>
+      <img src={props.image} alt="" class="w-full h-40 object-cover rounded-t-lg" />  
+      <CardTitle>{props.title}</CardTitle>
+      <CardDescription>{props.description}</CardDescription>
+    </CardHeader>
+    <CardContent>
+        <!--<div class ="mt-2">{props.ingredients}</div>-->
+        {props.ingredients}
+    </CardContent>
+    <CardFooter>
+      <button type="button" class="bg-blue-100 p-3">Add to Cart</button>
+    </CardFooter>
+</Card>
---
+++ src/routes/viewMenu/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+    import MenuUser from "$lib/components/ui/card/MenuUser.svelte";
+    import Taginput from '$lib/components/Taginput.svelte'
+</script>
+
+
+<div class="fonts-[Poppins] bg-gray-600 h-screen">
+    <header class="bg-white h-[10%]">
+        <nav class="flex justify-between items-center w-[92%] mx-auto">
+            <div class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
+                <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+                    <li>
+                        <a class="hover:text-gray-500 text-xl p-2" href = "/#">Profile</a> 
+                    </li>
+                    <li>
+                        <a class= "hover:text-gray-500 text-xl p-2" href = "/#">Streak</a> 
+                        </li>
+                        <li>
+                        <a class= "bg-gray-500 text-white text-xl rounded-full p-2" href = "/#">Order</a> 
+                        </li>
+                        <li>
+                        <a class= "hover:text-gray-500 text-xl p-2" href = "/#">Upload</a> 
+                        </li>
+                </ul>
+            </div>
+            </nav></header>
+<MenuUser
+title="Pasta"
+description="RM10.00"
+image={"$lib/components/ui/card/placeholder.jpeg"}
+ingredients="apple,sugar";
+/>
+</div>
+            
