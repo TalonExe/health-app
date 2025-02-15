9fcbbc1d1627beefbd081d7e7b85773a72d83981
TalonExe <liew321@hotmail.com>
2025-02-15 14:34:51 +0800

    small changes

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,9 +1,37 @@
 <script>
     let i = 0
-</script>
+    import { getAuth, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+	import { onSnapshotsInSync } from "firebase/firestore";
+    const provider = new GoogleAuthProvider();
+    
+    const auth = getAuth();
 
+    const onSignIn = () => {
+    signInWithRedirect(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+    });
+    }
+
+    </script>
 <div>
     <p>{i}</p>
     <p>this is the home page</p>
     <button on:click={() => i++}>plyu</button>
+    <button on:click={() => onSignIn()}>Sign up</button>
 </div>
