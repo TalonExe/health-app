5692f81f80a0c00e6a2f9107e392d0175f8f6086
TalonExe <liew321@hotmail.com>
2025-02-15 11:02:50 +0800

    added melt ui

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let i = 0
+</script>
+
+<div>
+    <p>{i}</p>
+    <p>this is the home page</p>
+    <button on:click={() => i++}>plyu</button>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,15 @@
 import adapter from '@sveltejs/adapter-vercel';
 
+import { preprocessMeltUI, sequence } from '@melt-ui/pp'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+preprocess: sequence([
+    // ... other preprocessors
+    preprocessMeltUI() // add to the end!
+  ])
 };
 
 export default config;
